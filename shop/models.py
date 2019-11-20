3b80bd458a3a32852a43e90cd965640a2f8f8fa7
X00148264 Bilal Mohamadzadeh <X00148264@computing.stu.it-tallaght.ie>
2019-11-20 11:59:55 +0000

    initial

---
+++ shop/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.urls import reverse
+#from imagekit.models import ImageSpecField
+#from imagekit.processors import ResizeToFill
+
+# Create your models here.
+class Category(models.Model):
+    name = models.TextField()
+    products = models.ManyToManyField('Product')
+
+    def get_products(self):
+        return Product.objects.filter(category=self)
+
+    def __str__(self):
+        return self.name 
+
+    def get_absolute_url(self):
+        return reverse('product_list_by_category',args=[self.id])
+
+    
+class Product(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+  #  image_thumbnail = ImageSpecField(source='image',
+        #processors=[ResizeToFill(90, 90)],
+        #format='JPEG',
+       # options={'quality': 60})
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    def __str__(self):
+        return self.name
